d824797ea62622746b4c940a4980013af3b3513e
Brody <90652549+B-Billings@users.noreply.github.com>
2023-03-07 15:07:21 -0500

    Add files via upload

---
+++ rokuMS/config/user.js
@@ -0,0 +1,10 @@
+const sqlcreds = {
+    connectionLimit : 10,
+    host            : 'localhost',
+    user            : 'root',
+    password        : '',
+    database        : 'db_rokuusers',
+    port            : 3306 //8889
+}
+
+module.exports = sqlcreds;
